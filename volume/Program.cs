0a34d2052c4246a6a6146136397dcac20b4c0c50
39320 <39320@DESKTOP-IJMGSHS>
2024-01-25 19:02:41 +0100

    Aggiungere i file di progetto.

---
+++ volume/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace volume
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            radio radio = new radio();
+
+            radio.volume = 100;
+
+            Console.WriteLine("{0}", radio.aumentavolume(radio.volume));
+
+            Console.WriteLine("{0}", radio.abbassavolume(radio.volume));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ volume/radio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace volume
+{
+    internal class radio
+    {
+        public float volume { get; set; }
+
+
+        public float aumentavolume(float volume)
+        {
+            volume = volume + (volume / 10);
+
+            return volume;
+        }
+
+        public float abbassavolume(float volume)
+        {
+            volume = volume - (volume / 10);
+
+            return volume;
+        }
+    }
+}
